d65b6f5f9b8dd1974d1759451da96e20e15331cd
isma91 <ismaydogmus@gmail.com>
2016-10-13 18:08:57 +0200

    sample of the config file

---
+++ config-sample.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ * This is a sample of the config file, rename it to config.php
+ * 
+ */
+return [
+    'host' => 'localhost',
+    'dbname' => 'conceptArtCeramic',
+    'user' => 'root',
+    'password' => '',
+    'maintenance' => 'false'
+];
