da69c9cf7c20c391ff82df0d83ca906993a481cb
isma91 <ismaydogmus@gmail.com>
2016-10-13 17:58:05 +0200

    the about page

---
+++ view/fr/site/about.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="description" content="<?php echo $this->site["description"] ?>" />
+    <title><?php echo $this->site["title"]; ?></title>
+    <?php foreach ($this->css as $css) {
+        echo $css . "\n";
+    } foreach ($this->js as $js) {
+        echo $js . "\n";
+    } ?>
+</head>
+<body>
+<?php include("menu.php"); ?>
+<div class="container">
+    Page sur le cini
+</div>
+</body>
+</html>
