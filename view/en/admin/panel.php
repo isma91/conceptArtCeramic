b433bc5e425533ff82310515fda6fd04c091a133
isma91 <ismaydogmus@gmail.com>
2016-10-13 17:55:22 +0200

    admin home page

---
+++ view/en/admin/panel.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="description" content="<?php echo $this->site["description"] ?>" />
+    <title><?php echo $this->site["title"]; ?></title>
+    <?php foreach ($this->css as $css) {
+        echo $css . "\n";
+    } foreach ($this->js as $js) {
+        echo $js . "\n";
+    } ?>
+</head>
+<body>
+<?php include("adminMenu.php"); ?>
+<div class="container">
+    <div class="row mui-panel">
+        <h2 class="title">Bonjour <?php echo $this->firstname; ?> <?php echo $this->lastname; ?></h2>
+    </div>
+    <div class="row failed">
+        <?php if ($this->error !== null) { echo $this->error; } ?>
+    </div>
+    <div class="row success">
+        <?php if ($this->success !== null) { echo $this->success; } ?>
+    </div>
+</div>
+</body>
+</html>
