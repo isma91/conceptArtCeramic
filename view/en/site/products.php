5735fda225561be0f5a579a775737d68955306f4
isma91 <ismaydogmus@gmail.com>
2016-10-13 22:43:19 +0200

    remove title on the picture

--- view/en/site/products.php
+++ view/en/site/products.php
@@ -15,51 +15,48 @@
 <div class="container">
     <div class="row">
         <?php
-        if(count($this->categoriesMaterialsProducts) !== 0) {
-            if ($this->func === "categories") {
-                foreach ($this->categoriesMaterialsProducts as $category => $array) {
-                    if (count($array) !== 0) {
-                        ?>
-                        <div class="col s12 m4 l3">
-                            <div class="card">
-                                <div class="card-image">
-                                    <a class="productImg" href="/products/<?php echo $array["slug"]; ?>" ><img class="responsive-img" src="../../media/img/product/<?php echo $array["id"] . "/" . $array["img"] ?>"></a>
-                                    <span class="card-title"><?php echo $category; ?></span>
-                                </div>
-                                <div class="card-content">
-                                    <p><span class="categoryName"><?php echo $category; ?></span> <span class="productCount"><?php echo "(" . $array["count"] . ")";?></span></p>
-                                </div>
+        if ($this->func == "categories") {
+            foreach ($this->categoriesMaterialsProducts as $category => $array) {
+                if (count($array) !== 0 && $array["count"] != 0) {
+                    ?>
+                    <div class="col s12 m4 l3">
+                        <div class="card">
+                            <div class="card-image">
+                                <a class="productImg" href="/products/<?php echo $array["slug"]; ?>" ><img class="responsive-img" src="../../media/img/product/<?php echo $array["id"] . "/" . $array["img"] ?>"></a>
+                            </div>
+                            <div class="card-content">
+                                <p><span class="categoryName"><?php echo $category; ?></span> <span class="productCount"><?php echo "(" . $array["count"] . ")";?></span></p>
                             </div>
                         </div>
-                        <?php
-                    }
+                    </div>
+                    <?php
                 }
-            } elseif ($this->func === "materials") {
-                foreach ($this->categoriesMaterialsProducts as $material => $array) {
-                    if (count($array["product"]) !== 0) {
-                        ?>
-                        <div class="col s12 m4 l3">
-                            <div class="card">
-                                <div class="card-image">
-                                    <a class="productImg" href="/products/<?php echo $array["categorySlug"] . "/" . $array["slug"]; ?>" ><img class="responsive-img" src="../../media/img/product/<?php echo $array["product"]["id"] . "/" . $array["product"]["img"] ?>"></a>
-                                    <span class="card-title"><?php echo $material; ?></span>
-                                </div>
-                                <div class="card-content">
-                                    <p><span class="materialName"><?php echo $material; ?></span> <span class="productCount"><?php echo "(" . $array["count"] . ")";?></span></p>
-                                </div>
+            }
+        } elseif ($this->func == "materials") {
+            foreach ($this->categoriesMaterialsProducts as $material => $array) {
+                if (count($array["product"]) !== 0) {
+                    ?>
+                    <div class="col s12 m4 l3">
+                        <div class="card">
+                            <div class="card-image">
+                                <a class="productImg" href="/products/<?php echo $array["categorySlug"] . "/" . $array["slug"]; ?>" ><img class="responsive-img" src="../../media/img/product/<?php echo $array["product"]["id"] . "/" . $array["product"]["img"] ?>"></a>
+                            </div>
+                            <div class="card-content">
+                                <p><span class="materialName"><?php echo $material; ?></span> <span class="productCount"><?php echo "(" . $array["count"] . ")";?></span></p>
                             </div>
                         </div>
-                        <?php
-                    }
+                    </div>
+                    <?php
                 }
-            } elseif ($this->func === "products") {
+            }
+        } elseif ($this->func == "products") {
+            if (count($this->categoriesMaterialsProducts) != 0) {
                 foreach ($this->categoriesMaterialsProducts as $product) {
                     ?>
                     <div class="col s12 m4 l3">
                         <div class="card">
                             <div class="card-image">
                                 <a class="productImg" href="/product/<?php echo $product["id"]; ?>" ><img class="responsive-img" src="../../media/img/product/<?php echo $product["id"] . "/" . $product["img"] ?>"></a>
-                                <span class="card-title"><?php echo $product["name"]; ?></span>
                             </div>
                             <div class="card-content">
                                 <p><span class="productName"><?php echo $product["name"]; ?></span></p>
@@ -68,6 +65,14 @@
                     </div>
                     <?php
                 }
+            } else {
+                ?>
+                <div class="col s12 m4 l3">
+                    <div class="row">
+                        <p class="title">No Product yet</p>
+                    </div>
+                </div>
+                <?php
             }
         }
         ?>
