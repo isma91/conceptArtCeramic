e5bf300c28748b32c8ce0b79b29fe1a2975d18f9
isma91 <ismaydogmus@gmail.com>
2016-10-13 18:02:38 +0200

    Customer controller to display the site

---
+++ controller/SiteController.php
@@ -0,0 +1,163 @@
+<?php
+
+namespace controller;
+
+use model\Bdd;
+use model\Config;
+use model\Product;
+use model\View;
+
+class SiteController
+{
+    public function checkMaintenance($page, array $array = array())
+    {
+        $config = new Config();
+        if ($config->getMaintenance() === "true") {
+            $view = new View("site#maintenance");
+            $view->render();
+        } else {
+            $view = new View($page);
+            if (!empty($array)) {
+                foreach ($array as $item => $value) {
+                    $view->set($item, $value);
+                }
+            }
+            $view->render();
+        }
+    }
+
+    public function index()
+    {
+        $this->checkMaintenance("site#index");
+    }
+
+    public function about()
+    {
+        $this->checkMaintenance("site#about");
+    }
+
+    public function contact()
+    {
+        $this->checkMaintenance("site#contact");
+    }
+
+    public function categories()
+    {
+        $bdd = new Bdd();
+        $categoriesArray = $bdd->select("category", array("*"));
+        $categories = array();
+        $productCount = array();
+        $productIdImg = array();
+        $field = $_SESSION["lang"] . "Name";
+        foreach ($categoriesArray as $category) {
+            $productCount = $bdd->select("product", array("COUNT(id)"), "idCategory LIKE '%" . $category["id"] . "%'");
+            $productIdImg[$category[$field]] = $bdd->select("product", array("id", "img"), "idCategory LIKE '%" . $category["id"] . "%'", null, "LIMIT 1");
+            $categories[$category[$field]]["slug"] = $category["slug"];
+            if (!empty($productCount)) {
+                $categories[$category[$field]]["count"] = $productCount[0]["COUNT(id)"];
+            }
+            if (!empty($productIdImg)) {
+                $img = explode("|", $productIdImg[$category[$field]][0]["img"]);
+                $categories[$category[$field]]["img"] = $img[0];
+                $categories[$category[$field]]["id"] = $productIdImg[$category[$field]][0]["id"];
+            }
+        }
+        $this->checkMaintenance("site#products", array("categoriesMaterialsProducts" => $categories, "func" => "categories"));
+    }
+
+    public function materials($category)
+    {
+        $bdd = new Bdd();
+        $id = $bdd->select("category", array("id"), "slug = '" . $category . "'");
+        if (!empty($id)) {
+            $idCategory = $id[0]["id"];
+            $slug = $bdd->select("category", array("slug"), "id = $idCategory")[0]["slug"];
+            $materialArray = $bdd->select("material", array("*"));
+            $materialsArray = array();
+            $field = $_SESSION["lang"] . "Name";
+            if (!empty($materialArray)) {
+                foreach ($materialArray as $array) {
+                    $materialsArray[$array["id"]] = array("name" => $array[$field], "slug" => $array["slug"]);
+                }
+            }
+            $materials = array();
+            foreach ($materialsArray as $idMaterial => $array) {
+                $materials[$array["name"]]["count"] = $bdd->select("product", array("COUNT(id)"), "idCategory LIKE  '%" . $idCategory . "%' AND idMaterial LIKE '%" . $idMaterial . "%'")[0]["COUNT(id)"];
+                $materials[$array["name"]]["id"] = $idMaterial;
+                $materials[$array["name"]]["slug"] = $array["slug"];
+                $materials[$array["name"]]["categorySlug"] = $slug;
+            }
+            foreach ($materials as $material => $array) {
+                $product = $bdd->select("product", array("id", "img"), "idCategory LIKE '" . $idCategory . "%' AND idMaterial LIKE '%" . $array["id"] . "%'", array(), "LIMIT 1");
+                if (!empty($product)) {
+                    $img = explode("|", $product[0]["img"])[0];
+                    $materials[$material]["product"] = array("id" => $product[0]["id"], "img" => $img);
+                }
+            }
+            $this->checkMaintenance("site#products", array("categoriesMaterialsProducts" => $materials, "func" => "materials"));
+        } else {
+            $this->categories();
+        }
+    }
+
+    public function products($category, $material)
+    {
+        $bdd = new Bdd();
+        $idCat = $bdd->select("category", array("id"), "slug = '" . $category . "'");
+        $idMat = $bdd->select("material", array("id"), "slug = '" . $material . "'");
+        if (!empty($idCat) && !empty($idMat)) {
+            $idCategory = $idCat[0]["id"];
+            $idMaterial = $idMat[0]["id"];
+            $field = $_SESSION["lang"] . "Name";
+            $products = array();
+            $where = "idCategory LIKE '%" . $idCategory . "%' AND idMaterial LIKE '%" . $idMaterial . "%'";
+            $productsArray = $bdd->select("product", array("id", "img", $field), $where);
+            foreach ($productsArray as $array) {
+                $img = explode("|", $array["img"])[0];
+                $products[] = array("id" => $array["id"], "img" => $img, "name" => $array[$field]);
+            }
+            $this->checkMaintenance("site#products", array("categoriesMaterialsProducts" => $products, "func" => "products"));
+        } else {
+            $this->categories();
+        }
+    }
+
+    public function product($id)
+    {
+        $productClass = new Product($id);
+        if (!is_null($productClass->getId())) {
+            $funcName = "get" . ucfirst($_SESSION["lang"]) . "Name";
+            $funcDescription = "get" . ucfirst($_SESSION["lang"]) . "Description";
+            $product = array();
+            $product["id"] = $productClass->getId();
+            $product["name"] = $productClass->$funcName();
+            $product["color"] = $productClass->getColor();
+            $product["material"] = $productClass->getMaterial();
+            $product["size"] = $productClass->getSize();
+            $product["usage"] = $productClass->getUsage();
+            $product["description"] = $productClass->$funcDescription();
+            $product["img"] = $productClass->getImg();
+            $this->checkMaintenance("site#product", array("product" => $product));
+        } else {
+            $this->categories();
+        }
+    }
+
+    public function sendMessage()
+    {
+        var_dump($_POST);
+    }
+
+    public function switchLanguage()
+    {
+        $lang = $_POST["language"];
+        $currentLang = $_SESSION["lang"];
+        if ($lang !== $currentLang) {
+            $_SESSION["lang"] = $lang;
+            sleep(1);
+            echo "true";
+        } else {
+            echo "false";
+        }
+    }
+}
