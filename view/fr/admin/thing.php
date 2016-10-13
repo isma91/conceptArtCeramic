96334dd1dbe4138352d46eb89afc48af9afc54bc
isma91 <ismaydogmus@gmail.com>
2016-10-13 17:56:22 +0200

    admin display all things

---
+++ view/fr/admin/thing.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="description" content="<?php echo $this->site["description"] ?>" />
+    <title><?php echo $this->site["title"]; ?></title>
+    <?php foreach ($this->css as $css) {
+        echo $css . "\n";
+    } foreach ($this->js as $js) {
+        echo $js . "\n";
+    } ?>
+</head>
+<body>
+<?php include("adminMenu.php"); ?>
+<div class="container">
+    <div class="row mui-panel" id="the_menu">
+        <h2 class="title">Voici les <?php echo $this->thing["title"]; ?> enregistrer</h2>
+    </div>
+    <div class="row failed">
+        <?php if ($this->error !== null) { echo $this->error; } ?>
+    </div>
+    <div class="row success">
+        <?php if ($this->success !== null) { echo $this->success; } ?>
+    </div>
+    <ul class="collection">
+        <?php
+        if (count($this->thing["thing"]) === 0) {
+            ?>
+            <li class="collection-item">Pas de <?php echo $this->thing["title"]; ?> enregistrer</li>
+            <?php
+        } else {
+            foreach ($this->thing["thing"] as $id => $name) {
+                ?>
+                <li class="collection-item center"><?php echo $name; ?><a class="btn waves-effect right" href="/admin/update/<?php echo $this->thing["slug"]; ?>/<?php echo $id; ?>">Modifier</a></li>
+                <?php
+            }
+        }
+        ?>
+    </ul>
+</div>
+</body>
+</html>
