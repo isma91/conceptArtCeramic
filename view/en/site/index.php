9fe6305f54d1e58c82ca68b556e1d924c77fde3c
isma91 <ismaydogmus@gmail.com>
2016-10-19 22:53:19 +0200

    display good img + diescription

--- view/en/site/index.php
+++ view/en/site/index.php
@@ -13,11 +13,17 @@
 <body>
 <?php include("menu.php"); ?>
 <div class="owl-carousel owl-theme" >
-    <div><img src="../../media/img/1.jpg"></div>
-    <div><img src="../../media/img/2.jpg"></div>
-    <div><img src="../../media/img/3.jpg"></div>
-    <div><img src="../../media/img/4.jpg"></div>
+    <div><img src="../../media/img/homePage/1.jpg"></div>
+    <div><img src="../../media/img/homePage/2.jpg"></div>
+    <div><img src="../../media/img/homePage/3.jpg"></div>
 </div>
-<div class="container"></div>
+<div class="container">
+    <div class="row center">
+        <p class="flow-text">Today, modern architecture has revived faience with simple and original models.</p>
+        <p class="flow-text">We have a quality collection, with respect for tradition and with a range of color and broad accessory.</p>
+        <p class="flow-text">Our company's aim is to discover our cultural and artistic works.</p>
+    </div>
+</div>
+<?php include("footer.html"); ?>
 </body>
 </html>
--- view/fr/site/index.php
+++ view/fr/site/index.php
@@ -13,11 +13,17 @@
 <body>
 <?php include("menu.php"); ?>
 <div class="owl-carousel owl-theme" >
-    <div><img src="../../media/img/1.jpg"></div>
-    <div><img src="../../media/img/2.jpg"></div>
-    <div><img src="../../media/img/3.jpg"></div>
-    <div><img src="../../media/img/4.jpg"></div>
+    <div><img src="../../media/img/homePage/1.jpg"></div>
+    <div><img src="../../media/img/homePage/2.jpg"></div>
+    <div><img src="../../media/img/homePage/3.jpg"></div>
 </div>
-<div class="container"></div>
+<div class="container">
+    <div class="row center">
+        <p class="flow-text">De nos jours, l'architechture moderne a fait revivre la faïence avec des models simples et originaux.</p>
+        <p class="flow-text">Nous avons une collection de qualité, dans le respect de la tradition et avec une gamme de couleur et accesoire très large.</p>
+        <p class="flow-text">Notre société a pour but de faire découvrir nos oeuvres culturel et artistiques.</p>
+    </div>
+</div>
+<?php include("footer.html"); ?>
 </body>
 </html>
