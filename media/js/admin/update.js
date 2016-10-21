9061fc92b970353d7284831b9fae59fe9994605c
isma91 <ismaydogmus@gmail.com>
2016-10-21 15:01:25 +0200

    adding switch + picture click to select checkbox

--- media/js/admin/update.js
+++ media/js/admin/update.js
@@ -6,4 +6,15 @@ $(document).ready(function () {
         $("#maintenance").prop('checked', false);
     }
     $("select").material_select();
+    $(document).on("change", "#addImg", function () {
+        if ($(this).prop('checked') === true) {
+            $('#imgField').html('<div class="row"><div class="file-field input-field"><div class="btn"><span>Image</span><input type="file" name="newImg[]" multiple></div><div class="file-path-wrapper"><input class="file-path validate" type="text"></div></div></div>');
+        } else {
+            $('#imgField').html('');
+        }
+    });
+    $(document).on("click", ".oldImg", function () {
+        var id = $(this).attr('checkbox');
+        $("[for='" + id + "']").click();
+    });
 });
