623d5148622781bf72350390661b4665a4e2d6eb
isma91 <ismaydogmus@gmail.com>
2017-04-29 23:12:45 +0200

    change file mode

---
+++ view/en/site/cini.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="description" content="<?php echo $this->site["description"] ?>" />
+    <title><?php echo $this->site["title"]; ?></title>
+    <?php foreach ($this->css as $css) {
+        echo $css . "\n";
+    } foreach ($this->js as $js) {
+        echo $js . "\n";
+    } ?>
+</head>
+<body>
+<?php include("menu.php"); ?>
+<div class="container">
+    <div class="row">
+        <h1 class="center">Çini</h1>
+    </div>
+</div>
+<ul class="pgwSlideshow">
+    <?php
+    $directory = __DIR__ . "/../../../media/img/homePage/";
+    $files = scandir($directory);
+    foreach ($files as $file) {
+        if ($file !== "." && $file !== ".." && $file !== "index.php") {
+            ?>
+            <li><img src="../../media/img/homePage/<?php echo $file; ?>"></li>
+            <?php
+        }
+    }
+    ?>
+</ul>
+<div class="container">
+    <div class="row center">
+        <p class="flow-text">Today, modern architecture has revived faience with simple and original models.</p>
+        <p class="flow-text">We have a quality collection, with respect for tradition and with a range of color and broad accessory.</p>
+        <p class="flow-text">Our company's aim is to discover our cultural and artistic works.</p>
+    </div>
+</div>
+<?php include("footer.html"); ?>
+</body>
+</html>
---
+++ view/fr/site/cini.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="description" content="<?php echo $this->site["description"] ?>" />
+    <title><?php echo $this->site["title"]; ?></title>
+    <?php foreach ($this->css as $css) {
+        echo $css . "\n";
+    } foreach ($this->js as $js) {
+        echo $js . "\n";
+    } ?>
+</head>
+<body>
+<?php include("menu.php"); ?>
+<div class="container">
+    <div class="row">
+        <h1 class="center">Çini</h1>
+    </div>
+</div>
+<ul class="pgwSlideshow">
+    <?php
+    $directory = __DIR__ . "/../../../media/img/homePage/";
+    $files = scandir($directory);
+    foreach ($files as $file) {
+        if ($file !== "." && $file !== ".." && $file !== "index.php") {
+            ?>
+            <li><img src="../../media/img/homePage/<?php echo $file; ?>"></li>
+            <?php
+        }
+    }
+    ?>
+</ul>
+<div class="container">
+    <div class="row center">
+        <p class="flow-text">De nos jours, l'architechture moderne a fait revivre la faïence avec des models simples et originaux.</p>
+        <p class="flow-text">Nous avons une collection de qualité, dans le respect de la tradition et avec une gamme de couleur et accesoire très large.</p>
+        <p class="flow-text">Notre société a pour but de faire découvrir nos oeuvres culturel et artistiques.</p>
+    </div>
+</div>
+<?php include("footer.html"); ?>
+</body>
+</html>
