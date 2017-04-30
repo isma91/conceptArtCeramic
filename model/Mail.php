143d107308bc86ac07a4a3405807048d6e1d6c46
isma91 <ismaydogmus@gmail.com>
2017-04-30 16:33:42 +0200

    add function who gonna send mail

---
+++ model/Mail.php
@@ -0,0 +1,54 @@
+<?php
+namespace model;
+
+
+class Mail
+{
+    public function sendMail($name, $email, $tel, $message, $idProduit = null)
+    {
+        $return = array("return" => "", "error" => "");
+        $messageClass = new Message();
+        $config = new Config();
+        $emailTo = $config->getEmailTo();
+        $messages = $messageClass->getMessages();
+        $subject = "Contact ConceptArtCeramic";
+        if (!empty($name) && !empty($email) && !empty($tel) && !empty($message)) {
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                $return["return"] = false;
+                $return["error"] = $messages["error"]["emailFormat"];
+                return $return;
+            }
+            if (!is_numeric($tel)) {
+                $return["return"] = false;
+                $return["error"] = $messages["error"]["telFormat"];
+                return $return;
+            }
+            $date = new \DateTime();
+            $datesend = $date->format("d-m-Y H:i:s");
+            $headers = "MIME-Version: 1.0\r\n";
+            $headers = $headers . "Content-Type: text/html; charset=ISO-8859-1\r\n";
+            if ($idProduit == null) {
+                $messageEmail = '<html><body><h1>Demande de Contact de "' . $name . '"</h1></br><h2>Email: "' . $email . '"</h2></br><h3>Message : "' . $message . '"</h3></body></html>';
+            } else {
+                $product = new Product($idProduit);
+                $productName = $product->getFrName();
+                $link = "www.conceptartceramic.com/product/" . $idProduit;
+                $messageEmail = '<html><body><h1>Demande de Contact de "' . $name . '"</h1></br><h2>Email: "' . $email . '"</h2></br><h3>Message : "' . $message . '"</h3></br><h4>Produit: <a href="' . $link . '">' . $productName . '</a></a></h4></body></html>';
+            }
+            $send = mail($emailTo, $subject, $messageEmail, $headers);
+            if($send) {
+                $return["return"] = true;
+                $return["error"] = "";
+                return $return;
+            } else {
+                $return["return"] = false;
+                $return["error"] = $messages["error"]["emptyField"];
+                return $return;
+            }
+        } else {
+            $return["return"] = false;
+            $return["error"] = $messages["error"]["emptyField"];
+            return $return;
+        }
+    }
+}
